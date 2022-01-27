72fe862c118a817e5a7b33483abb01f8d83e5b92
Ken <kennethkopelson@gmail.com>
2022-01-27 09:52:22 -0800

    connected mongoose to Mongodb database

--- server.js
+++ server.js
@@ -13,6 +13,62 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// Mongoose connection to MongoDB database.
+const db = require("./models");
+const dbConfig = require('./config/db.config');
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(()=>{
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+// This helps create 3 important rows in roles collection.
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0){
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "moderator"
+              }).save(err => {
+                if (err) {
+                  console.log("error", err);
+                }
+        
+                console.log("added 'moderator' to roles collection");
+              });
+        
+              new Role({
+                name: "admin"
+              }).save(err => {
+                if (err) {
+                  console.log("error", err);
+                }
+        
+                console.log("added 'admin' to roles collection");
+              });
+        }
+    });
+}
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Hello World." });
